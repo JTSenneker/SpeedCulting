78832f0d2cee284018519ebf91a8d20adc7e0364
Shane W. Myers <myerss12@ferris.edu>
2016-01-30 03:22:23 -0500

    xml added for profiles

--- SpeedCulting/Assets/Scripts/GameController.cs
+++ SpeedCulting/Assets/Scripts/GameController.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Text;
+
 
 public class GameController : MonoBehaviour {
 
     GameObject target;
     GameObject selves;
+
+    //GameObject[] profiles;
+
+    public TextAsset profileXml;
+
+    List<Dictionary<string, string>> profiles = new List<Dictionary<string, string>>();
+    Dictionary<string, string> obj;
+
     void Awake()
     {
         target = GameObject.Find("ProfileButtons");
         selves = GameObject.Find("ActionButtons");
+        getXml();
     }
 
 	// Use this for initialization
@@ -21,4 +35,32 @@ public class GameController : MonoBehaviour {
         target.SetActive(!target.activeSelf);
         selves.SetActive(!selves.activeSelf);
     }
+
+    void getXml()
+    {
+        XmlDocument profileDoc = new XmlDocument();
+        profileDoc.LoadXml(profileXml.text);
+        XmlNodeList profileNodes = profileDoc.GetElementsByTagName("profile");
+
+        foreach (XmlNode p in profileNodes)
+        {
+            XmlNodeList profileContent = p.ChildNodes;
+            obj = new Dictionary<string, string>();
+
+            foreach(XmlNode profInfo in profileContent){
+                if (profInfo.Name == "name")
+                {
+                    obj.Add("name", profInfo.InnerText);
+                }
+
+                profiles.Add(obj);
+            }
+        }
+
+        string nameProf;
+        profiles[0].TryGetValue("name", out nameProf);
+
+        print(nameProf);
+    }
+
 }
---
+++ SpeedCulting/Assets/Scripts/Profiles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Profiles : MonoBehaviour {
+
+    int affection = 0;
+    int suspicion = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
